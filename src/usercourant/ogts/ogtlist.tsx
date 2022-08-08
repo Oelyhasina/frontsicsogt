c98a42bbd0e4a48fdf1ece4702d7d1e5ca69b01b
Oelyhasina <hasina.oely@gmail.com>
2022-08-08 16:43:58 +0300

    Update ogtlist.tsx

--- src/usercourant/ogts/ogtlist.tsx
+++ src/usercourant/ogts/ogtlist.tsx
@@ -1,48 +1,46 @@
 import {
-    Create,
-    Datagrid,
-    Edit,
-    EditButton,
-    List,
-    SimpleForm,
-    TextField,
-    NumberInput,
-    TextInput,
-    SelectInput,
-    ReferenceInput,
-    NumberField,
-    Filter,
-    ReferenceField,
-    ReferenceArrayInput,
-    ChipField,
-    SelectArrayInput
-  } from 'react-admin';
+  Create,
+  Datagrid,
+  Edit,
+  EditButton,
+  List,
+  SimpleForm,
+  TextField,
+  NumberInput,
+  TextInput,
+  SelectInput,
+  ReferenceInput,
+  NumberField,
+  Filter,
+  ReferenceField,
+  ReferenceArrayInput,
+  ChipField,
+  SelectArrayInput
+} from 'react-admin';
 
-  const OgtFilter = (props : any) => (
-    <Filter {...props}>
-        <ReferenceArrayInput source="periodeId" reference="periode" alwaysOn>
-          <SelectArrayInput optionText="periode" />
-        </ReferenceArrayInput>
-        <ReferenceInput label="Categorie" source="categorieId" reference="categorie" alwaysOn>
-            <SelectInput optionText="intitule" />
-        </ReferenceInput>
-    </Filter>
-  )
+const OgtFilter = [
+      <ReferenceArrayInput source="periodeId" reference="periode" alwaysOn>
+        <SelectArrayInput optionText="periode" />
+      </ReferenceArrayInput>,
+      <ReferenceInput label="Categorie" source="categorieId" reference="categorie" alwaysOn>
+          <SelectInput optionText="intitule" />
+      </ReferenceInput>
+]
+
 export const OgtList = (props: any) => (
-    <List {...props} filters={<OgtFilter />}>
-      <Datagrid>
-        <TextField source="id"/>
-        <ReferenceField source="rubriqueId" reference="rubrique">
-          <TextField source="libelle" />
-        </ReferenceField>
-        <ReferenceField label="Codage" source="rubriqueId" reference="rubrique">
-          <TextField source="codage" />
-        </ReferenceField>
-        <NumberField source="montant" />
-        <ReferenceField label="Periode" source="periodeId" reference="periode">
-          <TextField source="periode" />
-        </ReferenceField>
-      </Datagrid>
-    </List>
+  <List {...props} filters={OgtFilter}>
+    <Datagrid>
+      <TextField source="id"/>
+      <ReferenceField source="rubriqueId" reference="rubrique">
+        <TextField source="libelle" />
+      </ReferenceField>
+      <ReferenceField label="Codage" source="rubriqueId" reference="rubrique">
+        <TextField source="codage" />
+      </ReferenceField>
+      <NumberField source="montant" />
+      <ReferenceField label="Periode" source="periodeId" reference="periode">
+        <TextField source="periode" />
+      </ReferenceField>
+    </Datagrid>
+  </List>
 );
-  
